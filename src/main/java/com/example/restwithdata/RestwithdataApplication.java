c873b07329eeebc269b257c68b0827411958a3fa
Ashish <jain1707@gmail.com>
2020-08-06 17:33:20 +0530

    Exclude config

--- src/main/java/com/example/restwithdata/RestwithdataApplication.java
+++ src/main/java/com/example/restwithdata/RestwithdataApplication.java
@@ -2,16 +2,20 @@ package com.example.restwithdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.cloud.aws.cache.config.annotation.CacheClusterConfig;
 import org.springframework.cloud.aws.cache.config.annotation.EnableElastiCache;
 import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		ContextStackAutoConfiguration.class,
+		ElastiCacheAutoConfiguration.class
+})
 @EnableRdsInstance(dbInstanceIdentifier="${cloud.aws.rds.dbInstanceIdentifier}", databaseName="${cloud.aws.rds.springaws.databaseName}",
 		username="${cloud.aws.rds.springaws.username}", password="${cloud.aws.rds.springaws.password}")
 @EnableElastiCache({@CacheClusterConfig(name = "myfirstcache", expiration = 36000)})
 public class RestwithdataApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestwithdataApplication.class, args);
 	}
